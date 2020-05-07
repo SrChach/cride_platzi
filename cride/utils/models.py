b389600038f63afc001046b505bdfbc66dc1d7ca
srchach <gsikmo@gmail.com>
2020-05-07 14:04:59 -0500

    ejemplo de proxy models y una herencia de modelos abstractos

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -30,3 +30,41 @@ class CRideModel(models.Model):
 
         get_latest_by = 'created'
         ordering = ['-created', '-modified']
+
+# Clase que hereda de una abstracta
+class Student(CRideModel):
+    name = models.CharField()
+
+    class Meta(CRideModel.META):
+        # Para cambiar algunas cosas, pero mantener las propiedades.
+        db_table = 'student_role'
+
+
+"""
+Ejemplo de Proxy
+
+Si queremos hacer cambios a la funcionalidad, sin implementar cambios
+en la base de datos, añadimos a META el atributo proxy, que no
+crea otra tabla pero permite extender funcionalidad
+"""
+class Person(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def say_hi(name):
+        pass
+
+# Llamando al manager del person original
+MyPerson.objects.all()
+
+ricardo = MyPerson.objects.get(pk=1)
+ricardo.say_hi('Pablo')
+
+"""
+Si llamas a los mismos métodos en el padre, no existen,
+por que no está extendida la funcionalidad
+"""
