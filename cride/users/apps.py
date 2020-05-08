e64390d988fc8c5eba8a4ed8897f23a09350857e
srchach <gsikmo@gmail.com>
2020-05-07 20:47:59 -0500

    Making changes to model of user to customize it

---
+++ cride/users/apps.py
@@ -0,0 +1,10 @@
+"""users app"""
+
+# Django
+from django.apps import AppConfig
+
+class UsersAppConfig(AppConfig):
+    """Users app config"""
+
+    name = 'cride.users'
+    verbose_name = 'Users'
---
+++ cride/users/models.py
@@ -0,0 +1,42 @@
+"""User model."""
+
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Utilities
+from cride.utils.models import CRideModel
+
+class User(CRideModel, AbstractUser):
+    """User model
+    
+    Extend from Django's Abstract User, change the username field
+    to email and add some extra fields 
+    """
+    email = models.EmailField(
+        'Email address',
+        unique=True,
+        error_messages={
+            'unique': 'A user with that email already exists.'
+        }
+    )
+
+    phone_number = models.CharField(max_length=17, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    is_client = models.BooleanField(
+        'client',
+        default=True,
+        help_text=(
+            'Help easily distinguish users and perform queries. '
+            'clients are the main type of user.'
+        )
+    )
+
+    is_verified = models.BooleanField(
+        'verified',
+        default=False,
+        help_text='Set to true when user have verified its email address.'
+    )
